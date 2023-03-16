2022ce1b730d5d77a982d63d1893530c4d8ca1f1
Alexandr <sasha-bragin@mail.ru>
2023-03-16 17:44:55 +0300

    Задача 41: Пользователь вводит с клавиатуры M чисел

---
+++ DZ1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры
+//  M чисел. Посчитайте, сколько чисел больше 0
+//   ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+
+Console.WriteLine("Введите числа для проверки в одну строку через пробел: ");
+string[] input = Console.ReadLine().Split(" ");
+int[] array = Array.ConvertAll(input, s => int.Parse(s));
+
+int Summa ()
+{
+int sum = 0;
+for (int index = 0; index < array.Length; index++)
+{
+  if (array[index] > 0)
+  {
+    sum++;
+  }
+}
+return sum;
+}
+
+Console.WriteLine($"Введены числа: {String.Join(", ", array)}");
+Console.WriteLine($"Из введенных количество чисел больше 0: {Summa()}");
+
+
+
+
+
+
+
+
+
